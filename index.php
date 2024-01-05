f5a24f7c1597678ae74aa5177ceefc2829a5dd33
ThalesSilva <125931825+ThalesHenriq@users.noreply.github.com>
2024-01-05 15:57:57 -0300

    new

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <header>
+        <nav>
+            <div id="menu_Align">
+                <ul>
+                    <li id="menu_Logo"></li>
+                    <li class="active"><a href="#">home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">contato</a></li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
